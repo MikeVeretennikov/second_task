c484cf190e76c59a8923c74372ed3865e7cf73c5
mike <msveretennikov@gmail.com>
2023-11-20 18:31:17 +0300

    create filesw

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        self.pushButton.clicked.connect(self.button)
+        # Обратите внимание: имя элемента такое же как в QTDesigner
+
+    def button(self):
+        pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
